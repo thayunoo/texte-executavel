05df6cde8d490c255c4a8afbe6db5ce4fd1d3eb2
Thayná Gomes da Silva <64287413+thayunoo@users.noreply.github.com>
2026-03-31 15:16:53 -0300

    Update teste_script.py

--- teste_script.py
+++ teste_script.py
@@ -1,2 +1,21 @@
-print("Executável funcionando no Windows!")
-input("Pressione Enter para sair...")
+import tkinter as tk
+from tkinter import messagebox
+
+def clicar():
+    messagebox.showinfo("Mensagem", "Funcionou! 🎉")
+
+# criar janela
+janela = tk.Tk()
+janela.title("Meu App Teste")
+janela.geometry("300x200")
+
+# texto
+label = tk.Label(janela, text="Teste de Aplicativo", font=("Arial", 12))
+label.pack(pady=20)
+
+# botão
+botao = tk.Button(janela, text="Clique aqui", command=clicar)
+botao.pack(pady=10)
+
+# rodar app
+janela.mainloop()
